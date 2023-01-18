7cc07050a1482fb300d53e3b0381a0b62131ec42
sujana10 <sujanasreenivasan@gmail.com>
2023-01-18 15:44:52 -0800

    Create SearchEngine.java

---
+++ SearchEngine.java
@@ -0,0 +1,11 @@
+import java.net.URI;
+
+public class SearchEngine implements URLHandler {
+
+    int num = 0;
+    String[] list = {"/add?s=anewstringtoadd", "/add?s=pineapple", "/add?s=apple", "/search?s=app"};
+
+
+    public String handleRequest(URI url) {
+        // code here
+    }
